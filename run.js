54a7944f3c3f7654c3642138dc9193348118faaf
Ken Smith <ken@onkea.com>
2016-07-20 19:40:12 -0400

    working goooooood

--- run.js
+++ run.js
@@ -1,7 +1,6 @@
+var mrequest = require('./mrequest.js');
 var config = require('../credentials/config.json');
 var mysql = require('mysql');
-
-console.log(config.mypassword);
 
 var con = mysql.createConnection({
     host: config.myhost,
@@ -12,3 +11,12 @@ var con = mysql.createConnection({
 
 con.connect();
 
+// Get the current date with leading zeros for mysql compare ex. 2015-12-28
+currentcrawl = dateFormat(now, "yyyy-mm-dd");
+
+// SQL for getting urls from the database
+getdomains = 'SELECT url FROM list LIMIT 100';
+
+// Run the SELECT query on mySQL database and get results or return err
+con.query(getdomains, function(err, rows, fields) {
+	if(err) throw err;
